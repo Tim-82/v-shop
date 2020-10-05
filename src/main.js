c7959cc2a28d76cc91757bf2de1e7d78d181ea15
Tim <you@example.com>
2020-10-05 20:53:06 +0300

    removed magnifier-mixin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './assets/styles/styles.scss'
+// import './assets/styles/styles.scss'
 import 'material-design-icons-iconfont'
 
 Vue.config.productionTip = false
