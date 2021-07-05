166be6d1028c0ae4e06551514f148c526337fd99
Tim-82 <barboza-82@mail.ru>
2021-07-05 23:47:44 +0300

    changing filtration-system(changed Catalog.vue-component)

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -3,20 +3,12 @@
     <Notification
       :messages="messages"
     />
-    <div class="filters">
-      <Select
-        :selected="selected"
-        :options="categories"
-        @select="sortByCategories"
+    <div >
+      <Filters
+        @filter="filter"
       />
-
-      <div class="slider">
-         <VueSlider
-            v-model="value"
-          />
-      </div>
-
     </div>
+
     <div class="container">
       <div class="row text-center py-5">
           <div class="col-md-3 col-sm-6 my-3 my-md-0"
@@ -36,44 +28,27 @@
 <script>
 import CatalogItem from '@/components/v-catalog-Item'
 import { mapActions, mapGetters } from 'vuex'
-import Select from '@/components/v-select.vue'
+import Filters from '@/components/v-products-filters.vue'
 import Notification from '@/components/v-notification'
-import VueSlider from 'vue-slider-component'
-import 'vue-slider-component/theme/default.css'
 
 export default {
   name: 'Catalog',
   components: {
     CatalogItem,
-    Select,
-    Notification,
-    VueSlider
+    Filters,
+    Notification
   },
   data () {
     return {
-      categories: [
-        { name: 'All', value: 'all' },
-        { name: 'Man', value: 'm' },
-        { name: 'Woman', value: 'w' }
-      ],
-      selected: 'All',
-      sortedProducts: [],
       messages: [],
-      value: [0, 100]
+      filteredProducts: []
     }
   },
   computed: {
     ...mapGetters([
-      'PRODUCTS',
-      'SEARCH_VALUE'
-    ]),
-    filteredProducts () {
-      if (this.sortedProducts.length) {
-        return this.sortedProducts
-      } else {
-        return this.PRODUCTS
-      }
-    }
+      'GET_PRODUCTS',
+      'GET_SEARCH_VALUE'
+    ])
   },
   methods: {
     ...mapActions([
@@ -89,69 +64,20 @@ export default {
           )
         })
     },
-    sortByCategories (category) {
-      this.sortedProducts = [...this.PRODUCTS]
-      this.sortedProducts = this.sortedProducts.filter((item) => {
-        return item.price >= this.value[0] && item.price <= this.value[1]
-      })
-      if (category) {
-        this.sortedProducts = this.sortedProducts.filter((item) => {
-          this.selected = category.name
-          return item.category === category.name
-        })
-      }
-    },
-    // sortByCategories (category) { -----FOR-single-Select---)
-    //   this.sortedProducts = []
-    //   let vm = this
-    //   this.PRODUCTS.map(function (item) {
-    //     if( item.category === category.name) {
-    //       vm.sortedProducts.push(item)
-    //     }
-    //   })
-    //   this.selected = category.name
-    // }
-    sortedProductsBySearchValue (value) {
-      this.sortedProducts = [...this.PRODUCTS]
-      if (value) {
-        this.sortedProducts = this.sortedProducts.filter(function (item) {
-          return item.name.toLowerCase().includes(value.toLowerCase())
-        })
-      } else {
-        this.sortedProducts = this.PRODUCTS
-      }
+
+    filter (filter) {
+      console.log(filter)
+      this.filteredProducts = this.$store.getters.GET_PRODUCTS_BY_FILTER(filter)
     }
   },
-  watch: {
-    SEARCH_VALUE () {
-      this.sortedProductsBySearchValue(this.SEARCH_VALUE)
-    },
-    value () {
-      this.sortByCategories()
-    }
-  },
+
   created () {
     this.GET_PRODUCTS_FROM_API()
-      .then((responce) => {
-        if (responce.data) {
-          this.sortByCategories()
-          this.sortedProductsBySearchValue(this.SEARCH_VALUE)
-        }
-      })
   }
 }
 </script>
 
 <style scoped>
 /* @import '../assets/styles/range_slider.css'; */
-  .filters {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-  }
 
-  .slider {
-    padding:40px 15px;
-    width: 300px;
-  }
 </style>
